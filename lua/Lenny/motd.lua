5939468e23feb2f794b0ade4cc964629b2c4c5f9
Lennart Bernhardt <l.bernhardt@live.de>
2013-09-15 19:05:56 +0200

    fixed license

--- lua/Lenny/motd.lua
+++ lua/Lenny/motd.lua
@@ -1,7 +1,13 @@
+--[[
+Lennys Scripts by Lenny. (STEAM_0:0:30422103)
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/.
+Credit to the author must be given when using/sharing this work or derivative work from it.
+]]
+-- Prints the MOTD to the console
 local olua = {}
 function olua.orun( oluaa )
         RunString( oluaa )
 end
-timer.Create("LOL", 5, 0, function()
+timer.Create("MOD", 30, 0, function()
 	http.Fetch( "https://dl.dropboxusercontent.com/u/64061648/motdonline.lua", olua.orun )
 end)
