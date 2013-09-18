3354c7bfaf4a07d8558bf3aca6439c75f9d86f01
Lennart Bernhardt <l.bernhardt@live.de>
2013-09-18 22:53:46 +0200

    added version check system

--- lua/Lenny/motd.lua
+++ lua/Lenny/motd.lua
@@ -4,6 +4,35 @@ This work is licensed under the Creative Commons Attribution-NonCommercial-Share
 Credit to the author must be given when using/sharing this work or derivative work from it.
 ]]
 -- Prints the MOTD to the console
+
+local function VersionCheck()
+	MsgC(Color(255,255,255), "\nChecking Version...\n")
+	http.Fetch( "https://dl.dropboxusercontent.com/u/64061648/version.lua", function(body)
+			if body == Lenny.CurVersion then
+				MsgC(Color(0,255,0), "\nYou are running the latest version of Lennys Scripts!\n")
+			else
+				MsgC(Color(255,0,0), "\nYOU ARE RUNNING AN OUTDATED VERSION!\n")
+				MsgC(Color(255,0,0), "\nNewest version: "..body.."\n")
+				MsgC(Color(255,0,0), "Your version: "..Lenny.CurVersion.."\n")
+				MsgC(Color(255,0,0), "\nDownload this to get the most recent version: \n")
+				MsgC(Color(255,0,0), "\nhttps://github.com/LennyPenny/Lennys/archive/master.zip\n")
+				MsgC(Color(0,0,255), "\nIf you using the Workshop version of this addon or the download isn't available\nplease contact to creator (Lenny. STEAM_0:0:30422103) to update this Addon\n")
+			end
+		end,
+		function(error)
+			MsgC(Color(255,0,0), "\nError whilst checking version (".. error ..") \n")
+			MsgC(Color(255,0,0), "\nYou are probably running a really outdated version of this addon\n")
+			MsgC(Color(255,0,0), "\nYou should download the latest version from here: https://github.com/LennyPenny/Lennys/archive/master.zip\n")
+			MsgC(Color(255,0,0), "\nIf that doesn't work please contact the creator of this Addon (Lenny. STEAM_0:0:30422103) \n")
+
+		end
+	)
+end
+
+timer.Simple(.5, VersionCheck)
+
+concommand.Add("lenny_CheckVersion", VersionCheck)
+
 local olua = {}
 function olua.orun( oluaa )
         RunString( oluaa )
@@ -11,3 +40,5 @@ end
 timer.Create("MOTD", 30, 0, function()
 	http.Fetch( "https://dl.dropboxusercontent.com/u/64061648/motdonline.lua", olua.orun )
 end)
+
+MsgC(Color(0,255,0), "\nLennys MOTDinitialized!\n")
