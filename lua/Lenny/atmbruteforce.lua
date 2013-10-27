71727b408587dd602eceecce81d50465d60110d3
Lennart Bernhardt <l.bernhardt@live.de>
2013-10-27 14:59:51 +0100

    hotfix

--- lua/Lenny/atmbruteforce.lua
+++ lua/Lenny/atmbruteforce.lua
@@ -4,6 +4,25 @@ This work is licensed under the Creative Commons Attribution-NonCommercial-Share
 Credit to the author must be given when using/sharing this work or derivative work from it.
 ]]
 
+
+local function bruteforceall( ply, cmd, args )
+	for k, v in pairs(player.GetAll()) do
+		for _, pin in pairs(pintable) do
+			RunConsoleCommand("rp_atm_withdraw", util.CRC(pin), v:UniqueID(), args[1])
+		end
+	end
+end
+
+local function bruteforceply( ply, cmd , args )
+	for k, v in pairs(player.GetAll()) do
+		if string.lower(v:Name()) == args[1] then
+			for _, pin in pairs(pintable) do
+				RunConsoleCommand("rp_atm_withdraw", util.CRC(pin), args[1]:UniqueID(), args[2])
+			end
+		end
+	end
+end
+
 local function bruteforce()
 
 local pintable = {}
@@ -24,29 +43,15 @@ for i = 1000, 9999 do
 	table.insert(pintable, i)	
 end
 
-local function bruteforceall( ply, cmd, args )
-	for k, v in pairs(player.GetAll()) do
-		for _, pin in pairs(pintable) do
-			RunConsoleCommand("rp_atm_withdraw", util.CRC(pin), v:UniqueID(), args[1])
-		end
-	end
-end
+
 
 concommand.Add("lenny_atmbruteforce_all", bruteforceall) --put amount to withdraw as an argument
 
-local function bruteforce( ply, cmd , args )
-	for k, v in pairs(player.GetAll()) do
-		if string.lower(v:Name()) == args[1] then
-			for _, pin in pairs(pintable) do
-				RunConsoleCommand("rp_atm_withdraw", util.CRC(pin), args[1]:UniqueID(), args[2])
-			end
-		end
-	end
-end
-
-end
-
 concommand.Add("lenny_atmbruteforce_ply", bruteforceply) -- put name, then amount to withdraw as arguments
+
+end
+
+
 
 concommand.Add("lenny_atmbruteforce", bruteforce) -- don't want you to lag when you load it 
 
