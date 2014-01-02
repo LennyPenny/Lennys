8a8033976a15140db69736a5a0664c450eade861
DaaOtt <daaott@gmail.com>
2014-01-01 18:55:27 -0800

    lastwep on propsurf

--- lua/Lenny/propsurf.lua
+++ lua/Lenny/propsurf.lua
@@ -7,12 +7,16 @@ Credit to the author must be given when using/sharing this work or derivative wo
 CreateClientConVar("lenny_propsurf_model", "models/props_c17/gravestone002a.mdl")
 
 local prop = GetConVarString("lenny_propsurf_model")
-
+local lastwep = ""
 
 local function startsurf()
 	local ang = LocalPlayer():EyeAngles()
 	local oriang = ang
 	LocalPlayer():SetEyeAngles(Angle(30, ang.yaw+180,0))
+	if LocalPlayer():GetActiveWeapon():GetClass() != "weapon_physgun" then
+		lastwep = LocalPlayer():GetActiveWeapon():GetClass()
+		RunConsoleCommand("use", "weapon_physgun")
+	end
 	timer.Simple(.1, function()
 		RunConsoleCommand("+attack")
 		RunConsoleCommand("gm_spawn", prop)
@@ -26,6 +30,9 @@ local function endsurf()
 	hook.Remove("CreateMove", "propsurf")
 	RunConsoleCommand("undo")
 	RunConsoleCommand("-attack")
+	if lastwep then
+		timer.Simple(0.1, function() RunConsoleCommand("use", lastwep) lastwep = "" end)
+	end
 end
 
 concommand.Add("+lenny_propsurf", startsurf)
