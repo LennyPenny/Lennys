f8b54391160e16af1b8265c2eed94f3d67adccb6
DaaOtt <daaott@gmail.com>
2014-03-02 20:00:15 -0800

    removed unneeded cvar

--- lua/Lenny/fovchanger.lua
+++ lua/Lenny/fovchanger.lua
@@ -1,10 +1,10 @@
 --[[
 Script by Lenny. (STEAM_0:0:30422103)
+Modified and improved by Ott (STEAM_0:0:36527860)
 This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/.
 Credit to the author must be given when using/sharing this work or derivative work from it.
 ]]
-CreateClientConVar("lenny_fov_on", 0)
-CreateClientConVar("lenny_fov", 100)
+CreateClientConVar("lenny_fov", 0)
 
 local newfov =  GetConVarNumber("lenny_fov")
 
@@ -22,7 +22,7 @@ end
 -- preperation
 hook.Remove("CalcView", "fov")
 timer.Simple(1, function()
-if GetConVarNumber("lenny_fov_on") == 1 then
+if GetConVarNumber("lenny_fov") != 0 then
 	hook.Add("CalcView", "fov", fov)
 end
 end)
@@ -30,10 +30,7 @@ end)
 
 cvars.AddChangeCallback("lenny_fov", function() 
 	newfov = GetConVarNumber("lenny_fov")
-end)
-
-cvars.AddChangeCallback("lenny_fov_on", function() 
-	if GetConVarNumber("lenny_fov_on") == 1 then
+	if newfov != 0 then
 		hook.Add("CalcView", "fov", fov)
 	else
 		hook.Remove("CalcView", "fov")
